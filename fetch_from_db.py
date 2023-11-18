ed911a25af28e7b329568380a3255b50deed07d9
duke-doki <145125297+duke-doki@users.noreply.github.com>
2023-11-18 19:04:48 +0300

    add id

--- fetch_from_db.py
+++ fetch_from_db.py
@@ -8,8 +8,8 @@ django.setup()
 from storage.models import Step
 
 
-def fetch_from_db(step, previous_step=None):
-    fetched_step = Step.objects.get(name=step)
+def fetch_from_db(step_id, previous_step_id=None):
+    fetched_step = Step.objects.get(id=step_id)
     name = fetched_step.name
     text = fetched_step.text
     buttons = fetched_step.buttons.all()
@@ -18,10 +18,10 @@ def fetch_from_db(step, previous_step=None):
         'text': text,
         'buttons': [button.text for button in buttons]
     }
-    if not previous_step:
+    if not previous_step_id:
         return step_params
     else:
-        fetched_step.previous = previous_step
+        fetched_step.previous = Step.objects.get(id=previous_step_id)
         fetched_step.save()
         step_params['previous'] = fetched_step.previous
         return step_params

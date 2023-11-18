7e98224757518d7eabfad0fbb22b6b89e47fc789
duke-doki <145125297+duke-doki@users.noreply.github.com>
2023-11-18 11:35:00 +0300

    fix functions

--- session.py
+++ session.py
@@ -14,8 +14,12 @@ def start_session(user):
     )
 
 
-#поменять session на юзера
-def add_step_to_session(session, step):
+def add_step_to_session(user, step):
+    session = Session.objects.get(user=user)
     session.steps.add(step)
 
-#добавить def remove_step_from_session(user, step)
+
+def remove_last_step_from_session(user):
+    session = Session.objects.get(user=user)
+    last_step = session.steps.last()
+    session.steps.remove(last_step)
